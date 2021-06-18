4e8e0d6c801a8c30ff7788f125345fe39162f73a
Nam G. VU <nam.gi.vu@gmail.com>
2021-06-18 13:56:53 +0700

    still fail mock environ[] by override bracket operator

--- tests/test_210606_mockito_envvar/test_210606_mockito_envvar.py
+++ tests/test_210606_mockito_envvar/test_210606_mockito_envvar.py
@@ -36,22 +36,16 @@ class Test(unittest.TestCase):
 
 
     def TODOtest_mock__os_environ_dict(self):
-        MOCKED_DICT = {
-            'SOME_VAR': 122,
-            'mocked key in os.environ 00': 1,
-            'mocked key in os.environ 02': '22',
-        }
+        MOCKED_VAL = 'some mocked value @ SOME_VAR'
+        MOCKED_DICT = {'SOME_VAR': MOCKED_VAL}
 
         # mock target code os.environ[:any]
-        when(os).environ[...].thenReturn(MOCKED_DICT)  #FIXME  #E       TypeError: 'StubbedInvocation' object is not subscriptable
+        # when(os).environ[...].thenReturn(MOCKED_DICT)  #FIXME  #E       TypeError: 'StubbedInvocation' object is not subscriptable
         # when2(os.environ).thenReturn(MOCKED_DICT)      #FIXME  #E       AttributeError: 'function' object has no attribute 'get'
         # patch(os.environ, lambda str: MOCKED_DICT)     #FIXME  #E       AttributeError: 'function' object has no attribute 'get'
+        # when(os.environ).__getitem__(...).thenReturn(MOCKED_VAL)  #FIXME this CANNOT get it mocked for environ[xx]  # mock environ[] by override bracket operator via __getitem__() ref. https://stackoverflow.com/a/1957793/248616
         self.addCleanup(unstub)  # register mockito's unstub() method to unittest's cleanup
 
         ACT_bybracket = os.environ['SOME_VAR']
         assert ACT_bybracket == MOCKED_DICT.get('SOME_VAR')
         assert ACT_bybracket != BEFORE_MOCKED_VAL
-
-        ACT_byget = os.environ.get('SOME_VAR')
-        assert ACT_byget == MOCKED_DICT.get('SOME_VAR')
-        assert ACT_byget != BEFORE_MOCKED_VAL
